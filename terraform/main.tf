4d2ac2aaac82aed9cde10e6608f331a47d1f80e6
Vivek <vivekops20@gmail.com>
2026-06-17 17:39:06 -0400

    updated ssh allow config

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,18 @@ resource "google_compute_instance" "vm" {
     }
   }
 
+  resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
   network_interface {
     network = google_compute_network.vpc.id
 
