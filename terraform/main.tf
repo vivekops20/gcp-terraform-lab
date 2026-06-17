78fe378c4048757613eb3f43fa089650ef428f03
Vivek <vivekops20@gmail.com>
2026-06-17 11:54:04 -0400

    initial terraform setup

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 7.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_network" "vpc" {
+  name = "terraform-vpc"
+}
+
+resource "google_compute_instance" "vm" {
+  name         = "terraform-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc.id
+
+    access_config {}
+  }
+}
